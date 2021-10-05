45dd831148518ebbf8bd1075c5ddb1c16994648b
Mathieu Ferraton <mferrato@redhat.com>
2021-10-05 08:30:34 -0700

    update deploy/cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-oc delete -f deploy/crds/konveyor.openshift.io_v1alpha1_velero_cr.yaml
-oc delete -f deploy/crds/konveyor.openshift.io_veleros_crd.yaml   
-oc delete -f deploy/non-olm/
+oc delete -f config/samples/
 oc delete namespace openshift-adp
 oc delete crd $(oc get crds | grep velero.io | awk -F ' ' '{print $1}')
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-oc create namespace openshift-adp
-oc create secret generic my-oadp-secret --namespace openshift-adp --from-file cloud=aws-credentials
-oc create -f deploy/non-olm
-oc create -f deploy/crds/konveyor.openshift.io_veleros_crd.yaml   
-oc create -f deploy/crds/konveyor.openshift.io_v1alpha1_velero_cr.yaml
+make deploy
+oc create secret generic cloud-credentials --namespace openshift-adp --from-file cloud=aws-credentials
+oc create -f serviceaccountvelero.yaml
+oc create -n openshift-adp -f config/samples/oadp_v1alpha1_velero.yaml
