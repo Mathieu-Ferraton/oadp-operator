22218d971cefb9c1f5f618fe0b2d628255fd1fc7
Mathieu Ferraton <mferrato@redhat.com>
2021-10-07 01:22:19 -0700

    deployment stuff

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 
 make deploy
 oc create secret generic cloud-credentials --namespace openshift-adp --from-file cloud=aws-credentials
-oc create -f serviceaccountvelero.yaml
 oc create -n openshift-adp -f config/samples/oadp_v1alpha1_velero.yaml
